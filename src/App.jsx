96650a6529909d9111ce36cfff17f2a67f1477c6
Abdul Adnan <peerabduladnan@gmail.com>
2025-07-04 14:26:14 +0530

    Created path for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { createBrowserRouter } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import Pastes from './Components/Pastes'
+import ViewPastes from './Components/ViewPastes'
 
 function App() {
 
+  let route = createBrowserRouter([
+    {
+      path: '/',
+      element: <div>
+        <Navbar />
+        <Home />
+      </div>
+    },
+    {
+      path: '/pastes',
+      element: <div>
+        <Navbar />
+        <Pastes />
+      </div>
+    },
+    {
+      path: '/pastes/:id',
+      element: <div>
+        <Navbar />
+        <ViewPastes />
+      </div>
+    }
+  ])
+
   return (
     <>
-    
+      <div>
+        Hello
+      </div>
     </>
   )
 }
---
+++ src/Components/ViewPastes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewPastes = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ViewPastes
